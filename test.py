6796ce60486516363c2c11d6765f59f26347acbc
gyxuan31 <gyxuan0131@gmail.com>
2025-06-04 22:12:37 -0400

    main

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 import numpy as np
-from scipy.optimize import minimize
-from statsmodels.tsa.arima_process import ArmaProcess
-import numpy as np
 import matplotlib.pyplot as plt
 import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
-from pyomo.core.expr.numeric_expr import Expr_if
+import sys
 np.random.seed(0)
 np.set_printoptions(threshold=np.inf)
 
@@ -19,22 +16,29 @@ B_total = 12*1e6 # total bandwidth/DU
 P_min = 3
 P_max = 6
 sigmsqr = -173 # dBm
+eta = 2
 
 # Rayleigh fading
 X = np.random.randn(num_DU, num_UE, num_RB) # real
 Y = np.random.randn(num_DU, num_UE, num_RB) # img
-H = (X + 1j * Y) / np.sqrt(2)   # H.shape = (num_UE, num_RB)
+H = (X + 1j * Y) / np.sqrt(2)   # H.shape = (num_DU, num_UE, num_RB)
 rayleigh_amplitude = np.abs(H)     # |h| Rayleigh(sigma=sqrt(1/2))
 rayleigh_gain = np.abs(H)**2          # |h|^2
+
+# Moving speed
+locux = np.random.randint(low=-10, high=10, size=(num_DU, num_UE)) # initialize users location x
+locuy = np.random.randint(low=-10, high=10, size=(num_DU, num_UE)) # initialize users location y
+locdux = np.zeros(num_DU) # initialize du location x
+locduy = np.zeros(num_DU) # initialize du location y
 
 e = np.zeros((num_DU, num_UE, num_RB))
 p = np.zeros((num_DU, num_UE, num_RB))
 
-demand = np.random.randint(low=0, high=5, size=num_UE)
+demand = np.random.randint(low=0, high=3, size=(num_DU, num_UE))
 
 # Model
 model = pyo.ConcreteModel()
-model.e = pyo.Var(range(num_DU), range(num_UE), range(num_RB), within=pyo.Binary)
+model.e = pyo.Var(range(num_DU), range(num_UE), range(num_RB), domain=pyo.Binary)
 model.p = pyo.Var(range(num_DU), range(num_UE), range(num_RB), domain=pyo.Reals)
 
 model.demand = pyo.Var(range(num_DU), range(num_UE), domain=pyo.Reals)
@@ -44,9 +48,23 @@ model.demandc = pyo.Constraint(range(num_DU), range(num_UE), rule=demandc)
 model.demandsatisfy = pyo.Constraint(range(num_DU), range(num_UE), 
                                  rule=lambda model,rho,u: sum(model.e[rho,u,k]for k in range(num_RB))>=model.demand[rho,u])
 
+model.locux = pyo.Var(range(num_DU), range(num_UE), domain=pyo.Reals)
+model.locuxc = pyo.Constraint(range(num_DU), range(num_UE), rule=lambda model,rho,u:
+    model.locux[rho,u] == locux[rho][u])
+model.locuy = pyo.Var(range(num_DU), range(num_UE), domain=pyo.Reals)
+model.locuyc = pyo.Constraint(range(num_DU), range(num_UE), rule=lambda model,rho,u:
+    model.locuy[rho,u] == locuy[rho][u])
+model.locdux = pyo.Var(range(num_DU), domain=pyo.Reals)
+model.locduxc = pyo.Constraint(range(num_DU), rule=lambda model,rho:
+    model.locdux[rho] == locdux[rho])
+model.locduy = pyo.Var(range(num_DU), domain=pyo.Reals)
+model.locduyc = pyo.Constraint(range(num_DU), rule=lambda model,rho:
+    model.locduy[rho] == locduy[rho])
+
 model.d = pyo.Var(range(num_DU), range(num_UE), range(num_RB), domain=pyo.Reals)
 def dc(model,rho,u,k):
-    return model.d[rho,u,k] == rayleigh_gain[rho][u][k]
+    return model.d[rho,u,k] == rayleigh_gain[rho][u][k]*\
+        pyo.sqrt((model.locux[rho,u]-model.locdux[rho])**2+(model.locuy[rho,u]-model.locduy[rho])**2+1e-6)**(-eta)
 model.dc = pyo.Constraint(range(num_DU), range(num_UE), range(num_RB), rule=dc) # d = d*h
 model.I = pyo.Var(range(num_DU), range(num_UE), range(num_RB), domain=pyo.Reals)
 def Ic(model, rho, u, k):
@@ -63,24 +81,31 @@ model.cuc = pyo.Constraint(range(num_DU), range(num_UE),
                              rule= lambda model,rho,u: 
                                 model.cu[rho,u] == sum(model.crb[rho,u,k] for k in range(num_RB)))
 
-model.minc = pyo.Var()
-model.mincc = pyo.Constraint(range(num_DU), range(num_UE), rule= lambda model,i: model.minc<=model.cu[i])
+model.minc = pyo.Var(range(num_DU))
+model.mincc = pyo.Constraint(range(num_DU), range(num_UE), rule= lambda model,rho,u: 
+    model.minc[rho] <= model.cu[rho,u])
 
 # constraints
-model.numrbc = pyo.Constraint(expr=sum(sum(model.e[u,k] for k in range(num_RB))for u in range(num_UE)) <=num_RB)
-model.pminc = pyo.Constraint(range(num_UE), range(num_RB), rule= lambda model, u, k:
-    model.p[u,k] >= P_min)
-model.pmaxc = pyo.Constraint(range(num_UE), range(num_RB), rule= lambda model, u, k:
-    model.p[u,k] <= P_max)
+model.numrbc = pyo.Constraint(range(num_DU), rule= lambda model,rho:
+    sum(sum(model.e[rho,u,k] for k in range(num_RB))for u in range(num_UE)) <=num_RB)
+model.pminc = pyo.Constraint(range(num_DU), range(num_UE), range(num_RB), rule= lambda model, rho, u, k:
+    model.p[rho,u,k] >= P_min)
+model.pmaxc = pyo.Constraint(range(num_DU), range(num_UE), range(num_RB), rule= lambda model, rho, u, k:
+    model.p[rho,u,k] <= P_max)
 
+for rho in range(num_DU):
+    print("num_DU:", rho)
+    model.obj = pyo.Objective(expr=model.minc[rho], sense=pyo.maximize)
+    opt = SolverFactory('ipopt')
+    f = open('log.txt', 'w')
+    sys.stdout = f
+    model.pprint()
+    f.close()
+    result = opt.solve(model, tee=True)
+    for i in range(num_UE):
+        for j in range(num_RB):
+            p[rho,i,j] = pyo.value(model.p[rho,i,j])
+            e[rho,i,j] = pyo.value(model.e[rho,i,j])
+    model.obj.deactivate()
 
-model.obj = pyo.Objective(expr=model.minc, sense=pyo.maximize)
-
-opt = SolverFactory('mindtpy')
-# opt.options['max_iter'] = 100
-result = opt.solve(model, tee=True)
-for i in range(num_UE):
-    for j in range(num_RB):
-        p[i,j] = pyo.value(model.p[i,j])
-        e[i,j] = pyo.value(model.e[i,j])
-print(p)
+print(e)
