390409e89bba30157839408ceaed6162734519f6
gyxuan31 <gyxuan0131@gmail.com>
2025-06-09 12:32:19 -0400

    update

---
+++ temp.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+from statsmodels.tsa.arima_process import ArmaProcess
+
+ar = np.array([1, -0.75, 0.25])
+ma = np.array([1, 0.65])
+arma = ArmaProcess(ar, ma)
+
+num_samples = 100
+length = 50
+dataset = np.zeros((num_samples, length))
+
+for i in range(num_samples):
+    dataset[i] = arma.generate_sample(nsample=length)
+
+X_train = []
+Y_train = []
+
+for series in dataset:
+    for t in range(2, length):
+        X_train.append([series[t-2], series[t-1]])
+        Y_train.append(series[t])
+
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
+
+kernel = RBF(length_scale=1.0)
+gp = GaussianProcessRegressor(kernel=kernel, alpha=1e-2)
+gp.fit(X_train, Y_train)
+
+test_series = arma.generate_sample(nsample=length)
+
+y0, y1 = test_series[0], test_series[1]
+predicted = [y0, y1]
+
+future_len = 20
+for _ in range(future_len):
+    x_input = np.array([[predicted[-2], predicted[-1]]])
+    y_next = gp.predict(x_input)[0]
+    predicted.append(y_next)
+
+plt.figure(figsize=(10, 4))
+plt.plot(range(len(test_series)), test_series, 'r--', label='True ARMA Sample')
+plt.plot(range(len(predicted)), predicted, 'b-', label='Predicted (GP Rolling)')
+plt.axvline(x=1, color='gray', linestyle='--', label='Prediction Start')
+plt.xlabel("Time Step")
+plt.ylabel("Value")
+plt.title("GP Rolling Forecast from ARMA Sequence")
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
